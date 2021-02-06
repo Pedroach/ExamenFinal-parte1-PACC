1bcf8e97ee284bdc72be29fb35ba326e22b15293
Pedro <a2019114887@estudiantes.edu.bo>
2021-02-05 21:27:21 -0400

    UnitTest realizado

--- WebSolExamenFinal/NUnitTest1/UnitTest1.cs
+++ WebSolExamenFinal/NUnitTest1/UnitTest1.cs
@@ -14,8 +14,8 @@ namespace NUnitTest1
         public void Letra()
         {
             //Arrange=preparacion
-            string num1 = "5";
-            string esperado = "Cinco";
+            string num1 = "1";
+            string esperado = "Uno";
             LetraController lit = new LetraController();
 
 
@@ -27,11 +27,46 @@ namespace NUnitTest1
 
 
         }
-        public void Letra1()
+        [Test]
+        public void Letra2()
         {
             //Arrange=preparacion
-            string num1 = "0";
-            string esperado = "Cero";
+            string num1 = "2";
+            string esperado = "Dos";
+            LetraController lit = new LetraController();
+
+
+            //Act=ejecution
+            string resultado = lit.numeroget(num1);
+
+            //Assert=Verification
+            Assert.AreEqual(esperado, resultado);
+
+
+        }
+        [Test]
+        public void Letra3()
+        {
+            //Arrange=preparacion
+            string num1 = "3";
+            string esperado = "Tres";
+            LetraController lit = new LetraController();
+
+
+            //Act=ejecution
+            string resultado = lit.numeroget(num1);
+
+            //Assert=Verification
+            Assert.AreEqual(esperado, resultado);
+
+
+        }
+        [Test]
+        public void Letra4()
+        {
+            //Arrange=preparacion
+            string num1 = "2";
+            string esperado = "Dos";
             LetraController lit = new LetraController();
 
 
